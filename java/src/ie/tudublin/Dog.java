e9a3bf69e0ad944502a6f82764fda97cba2e379a
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-01-23 16:23:01 +0000

    Lect 1: Java program example

---
+++ java/src/ie/tudublin/Dog.java
@@ -0,0 +1,22 @@
+package ie.tudublin;
+
+public class Dog
+{
+    String name;
+    public Dog() {
+
+    }
+    public Dog(String name) {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(String name) {
+        return name;
+    }
+    public void bark() {
+        System.out.println("Woof");
+    }
+}
