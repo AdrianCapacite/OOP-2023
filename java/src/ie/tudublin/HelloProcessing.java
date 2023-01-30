8895c576fcefa7deb4e5489ffa78b0c3bfc6e2fb
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-01-30 14:02:48 +0000

    ahh

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -14,19 +14,22 @@ public class HelloProcessing extends PApplet
 	{
 		// colorMode(HSB);
 		color(RGB);
-		background(0);
+		// background(0);
+		background(255,0,0);
 	}
 
 	public void draw()
 	{
-		noFill();
+		// int eyeSize = (mouseY + 250) / 50;
+		int eyeSize = 1;
 		noStroke();
-		strokeWeight(10);
-		stroke(255);
-		fill(255,0,255);
-		line(10, 10, 100, 100); // x1, y1, x2, y2
-		circle(300, 300, 50); // x, y, radius
-		rect(400, 200, 400, 200); // x, y, width, height
-		triangle(100, 100, 200, 100, 150, 200); // x1, y1, x2, y2, x3, y3
+		fill(255,255,0);
+		circle(250,300,400);
+		fill(0,255,255);
+		triangle(250, 0, 50, 450, 450, 450); // x1, y1, x2, y2, x3, y3
+		fill(255/2);
+		ellipse(250, 250, 200, 100/eyeSize); // x, y, width, height
+		fill(0);
+		circle(250,250,80/eyeSize);
 	}
 }
