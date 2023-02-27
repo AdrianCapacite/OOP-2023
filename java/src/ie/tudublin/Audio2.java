21026bc8803d6bb6897ad099675adf033de93a4e
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-02-27 11:30:13 +0000

    create Audio2

---
+++ java/src/ie/tudublin/Audio2.java
@@ -0,0 +1,21 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+import ddf.minim.AudioInput;
+
+public class Audio2 extends PApplet{
+   public void settings()
+   {
+       size(1024, 1024);
+   }
+
+    public void setup()
+    {
+         colorMode(HSB);
+         background(0);
+    }
+
+    public void draw()
+    {
+    }
+}
