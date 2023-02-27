e582b60c5a35f28403b5f2b6dbcbe9bdaa579a3a
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-02-27 11:35:58 +0000

    Settings and notes

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -1,11 +1,17 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+// import ddf.minim.*;
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 
+/**
+ * Class notes:
+ *  - Abstract class = class that cannot be instantiated but can be extended
+ *      - E.g PApplet
+ */
 public class Audio2 extends PApplet{
     Minim minim;
     AudioInput ai;
@@ -14,7 +20,11 @@ public class Audio2 extends PApplet{
 
     public void settings()
     {
-       size(1024, 1024);
+        size(1024, 1024);
+        minim = new Minim(this);
+        ai = minim.getLineIn(Minim.STEREO, width, 44100, 16);
+        ab = ai.mix; // get the mixed audio buffer
+
     }
 
     public void setup()
