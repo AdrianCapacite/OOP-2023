61ea8d26bf3319674b58a88664de93dfd81571be
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-03-01 16:11:20 +0000

    Perlin Lines modify appearance

--- java/src/ie/tudublin/PerlinLines.java
+++ java/src/ie/tudublin/PerlinLines.java
@@ -13,14 +13,14 @@ public class PerlinLines extends Base {
         frameRate(60);
         surface.setTitle("Perlin Lines");
 
-        lines = 50;
+        lines = 30;
     }
 
     public void draw() {
         background(0);
         text(frameCount * 0.01f, 10, 10);
         stroke(255);
-        strokeWeight(2);
+        strokeWeight(3);
         noFill();
         for (int i = 0; i < lines; i++) {
             float x1 = constrain(map(i,0,lines/2,0,width), 0, width);
@@ -29,18 +29,17 @@ public class PerlinLines extends Base {
             float y2 = constrain(map(i,0,lines/2,0,height), 0, height);
             beginShape();
             int sections = (int) dist(x1, y1, x2, y2)/10;
-            float scale = 0.0005f; // Smaller number = more detail
+            float scale = 0.0010f; // Smaller number = more detail
             float timeMod = 0.005f;
             for (int j = 0; j < sections; j++) {
                 float x = map(j, 0, sections-1, x1, x2);
                 float y = map(j, 0, sections-1, y1, y2);
-                float displacement = map(noise(x * scale, y * scale, frameCount * timeMod), 0, 1, -100, 100);
+                float displacement = map(noise(x * scale, y * scale, frameCount * timeMod), 0, 1, -500, 500);
                 float mouseDist = dist(mouseX, mouseY, x, y);
                 // y += displacement ;
                 float j2 = map(j, 0, sections+1, -1, 1);
                 float distToEdge = min(min(x, y), min(width-x, height-y));
-                float dtemap = map(distToEdge, 0, min(width/2, height/2)/2, 1, 0);
-                y += displacement * constrain(0-(dtemap * dtemap)*4+2,0,1);
+                y += displacement * constrain((distToEdge * distToEdge)*2/262144,0,1);
                 // y += displacement * constrain(1 - mouseDist/200,0,1);
                 curveVertex(x, y);
             }
