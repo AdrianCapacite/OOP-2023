46c8946d481e65dd8e93e4f6d42f946fe42d7968
Adrian Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-01 17:02:13 +0000

    Implemented PitchSpeller

--- java/src/ie/tudublin/PitchSpeller.java
+++ java/src/ie/tudublin/PitchSpeller.java
@@ -3,10 +3,23 @@ package ie.tudublin;
 public class PitchSpeller {
     // Variables
     // Lowest note: C0 16.35Hz
-    // Multiplier: 1.0594630943592953
-    public char spell(float freq) {
-        float indexF = (float) (Math.log(freq / 16.35) / Math.log(1.0594630943592953));
-        int index = (int) Math.round(indexF) % 12;
-        return (char) (index + 65);
+    float C0 = 16.35f;
+    float coeff = 1.0594630943592953f;
+    String[] noteNames = {"C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"};
+
+    /**
+     * Get position of note in musical frequency range
+     * @param freq
+     * @return
+     */
+    public int getNote(float freq) {
+        float indexF = (float) (Math.log(freq / C0) / Math.log(coeff));
+        int index = (int) Math.round(indexF);
+        return index;
+    }
+
+    public String spell(float freq) {
+        int note = getNote(freq);
+        return noteNames[note % 12];
     }
 }
