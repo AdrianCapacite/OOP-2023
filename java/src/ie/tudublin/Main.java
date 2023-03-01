4301d0ccfe1a2ad35856dc4ef77291517f076f88
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-03-01 01:11:00 +0000

    Added Base PApplet

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,8 +2,14 @@ package ie.tudublin;
 
 public class Main
 {
-	
+	static void base(){
+		String a = "MAIN";
+		processing.core.PApplet.runSketch(new String[] {a}, new Base());
+	}
 	public static void main(String[] args)
 	{
+		System.out.println("Program Started");
+		base();
+		System.out.println("Program Ended");
 	}
 }
