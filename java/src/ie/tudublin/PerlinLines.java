aa902e2667ef58512d40077cced59cd2c33fa852
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-03-01 02:18:47 +0000

    Perlin Lines

--- java/src/ie/tudublin/PerlinLines.java
+++ java/src/ie/tudublin/PerlinLines.java
@@ -32,15 +32,20 @@ public class PerlinLines extends Base {
             float scale = 0.0005f; // Smaller number = more detail
             float timeMod = 0.005f;
             for (int j = 0; j < sections; j++) {
-                float x = map(j, -1, sections, x1, x2);
-                float y = map(j, -1, sections, y1, y2);
-                float displacement = map(noise(x * scale, y * scale, frameCount * timeMod), 0, 1, -500, 500);
+                float x = map(j, 0, sections-1, x1, x2);
+                float y = map(j, 0, sections-1, y1, y2);
+                float displacement = map(noise(x * scale, y * scale, frameCount * timeMod), 0, 1, -100, 100);
                 float mouseDist = dist(mouseX, mouseY, x, y);
-                y += displacement;
+                // y += displacement ;
+                float j2 = map(j, 0, sections+1, -1, 1);
+                float distToEdge = min(min(x, y), min(width-x, height-y));
+                float dtemap = map(distToEdge, 0, min(width/2, height/2)/2, 1, 0);
+                y += displacement * constrain(0-(dtemap * dtemap)*4+2,0,1);
                 // y += displacement * constrain(1 - mouseDist/200,0,1);
                 curveVertex(x, y);
             }
             endShape();
+            text(min(min(mouseX, mouseY), min(width-mouseX, height-mouseY)), mouseX, mouseY);
         }
     }
 }
