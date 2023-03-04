91ea2cb90c79b8a19ebd28d4a037639d462e2b79
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-04 19:29:15 +0000

    Cleared out files

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,36 +3,6 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
-	public static void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-
-
-	public static void audio1()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new SegmentationFault());
-    }
 
 	public static void osci() {
 		String[] a = {"MAIN"};
@@ -41,12 +11,8 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
-
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
-
+		System.out.println("Program Begin");
 		osci();
+		System.out.println("Program End");
 	}
 }
