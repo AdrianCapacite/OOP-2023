ee637a8f785f0fa6bd5f9f3638e0a4a7e50c0e4d
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-08 16:33:27 +0000

    Implemented Controls

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -4,8 +4,8 @@ import processing.core.PApplet;
 
 public class Life extends PApplet
 {
-
 	LifeBoard board;
+    boolean running;
 	public void settings()
 	{
 		size(1024, 1024);
@@ -16,13 +16,38 @@ public class Life extends PApplet
 		background(0);
 		board = new LifeBoard(50, this);
 		board.randomise();
+        running = true;
 	}
 
 	public void draw()
 	{
 		background(0);
 		board.render();
-		board.applyRules();
+
+        if (running)
+        {
+            board.applyRules();
+        }
 
 	}
+
+    public void keyPressed()
+    {
+        switch(key)
+        {
+            case ' ':
+                running = !running;
+                break;
+            case '1':
+                board.randomise();
+                break;
+            case '2':
+                board.clear();
+                break;
+            case '3':
+                // Set corss
+                break;
+        }
+
+    }
 }
